c075db74c5764c065f5bf689b6d8e1e589795762
Alex <alex.cornelio@covergenius.com>
2021-08-07 09:59:26 +1000

    Install precommit with black, flake8, isort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 from views import router
 
 app = FastAPI()
--- views.py
+++ views.py
@@ -1,13 +1,14 @@
 from io import StringIO
 
 import pandas as pd
-from fastapi import File, UploadFile, Request, APIRouter
+from fastapi import APIRouter, File, Request, UploadFile
 from fastapi.responses import HTMLResponse
 
 from spend_calendar import SpendCalendar
 from templates import templates
 
 router = APIRouter()
+
 
 @router.get("/", response_class=HTMLResponse)
 async def home(request: Request):
@@ -19,14 +20,24 @@ async def upload(request: Request, file: UploadFile = File(...)):
 
     # save file to dir, validate
     data = await file.read()
-    df = pd.read_csv(StringIO(data.decode('utf-8')), header=None, delimiter=",", usecols=[0, 1, 2], names=["date", "amount", "description"], parse_dates=['date'], dayfirst=True)
+    df = pd.read_csv(
+        StringIO(data.decode("utf-8")),
+        header=None,
+        delimiter=",",
+        usecols=[0, 1, 2],
+        names=["date", "amount", "description"],
+        parse_dates=["date"],
+        dayfirst=True,
+    )
 
     # pd
-    daily_amount_sum = df.resample('D', on='date').amount.sum()
+    daily_amount_sum = df.resample("D", on="date").amount.sum()
     cal = SpendCalendar(daily_amount_sum, 7, 2021)
 
     # pick first month year
-    first_year_month = (2021, 7)
+    # first_year_month = (2021, 7)
     html_calendar = cal.formatmonth(2021, 7)
 
-    return templates.TemplateResponse("index.html", {"request": request, "calendar": html_calendar})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "calendar": html_calendar}
+    )
